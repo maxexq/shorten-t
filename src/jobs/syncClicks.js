a9244d302f3f2dd97750dbae65571e4abf4f5b94
maxexq <parames.ajh@gmail.com>
2026-02-05 21:35:41 +0700

    fix: change cron from every 5mins to every day cause Vercel limit

--- src/jobs/syncClicks.js
+++ src/jobs/syncClicks.js
@@ -2,7 +2,7 @@ const cron = require("node-cron");
 const urlService = require("../services/urlService");
 
 // Sync clicks to MongoDB every 5 minutes
-cron.schedule("*/5 * * * *", async () => {
+cron.schedule("0 0 * * *", async () => {
   console.log("Syncing clicks to database...");
   await urlService.syncClicksToDatabase();
 });
